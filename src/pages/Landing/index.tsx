f56bcf6b21c8b4707b2edf855abbc89f7423d1c3
Rafael Dias <rafaeldiasdesouza.rd@gmail.com>
2020-08-04 23:29:21 -0300

    feat: testing commits

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React from 'react';
 
-import logoImg from "../../assets/images/logo.svg";
-import landingImg from "../../assets/images/landing.svg";
+import logoImg from '../../assets/images/logo.svg';
+import landingImg from '../../assets/images/landing.svg';
 
-import studyIcon from "../../assets/images/icons/study.svg";
-import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
-import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
+import studyIcon from '../../assets/images/icons/study.svg';
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
-import "./styles.css";
+import './styles.css';
 
-const Landing = () => (
+const Landing: React.FC = () => (
   <div id="page-landing">
     <div id="page-landing-content" className="container">
       <div className="logo-container">
@@ -22,17 +22,17 @@ const Landing = () => (
         className="hero-image"
       />
       <div className="buttons-container">
-        <a href="" className="study">
+        <a href="/" className="study">
           <img src={studyIcon} alt="Estudar" />
           Estudar
         </a>
-        <a href="" className="give-classes">
+        <a href="/" className="give-classes">
           <img src={giveClassesIcon} alt="Dar aulas" />
           Dar aulas
         </a>
       </div>
       <span className="total-connections">
-        Total de 200 conexões já realizadas{" "}
+        Total de 200 conexões já realizadas
         <img src={purpleHeartIcon} alt="Coração roxo" />
       </span>
     </div>
