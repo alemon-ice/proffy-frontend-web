904dc7551a3961b591621db3186dc0e3fd061372
Rafael Dias <rafaeldiasdesouza.rd@gmail.com>
2020-08-05 21:42:14 -0300

    feat: add routes

--- src/__tests__/pages/Landing.test.tsx
+++ src/__tests__/pages/Landing.test.tsx
@@ -3,6 +3,12 @@ import Landing from 'pages/Landing';
 
 import { render } from '@testing-library/react';
 
+jest.mock('react-router-dom', () => {
+  return {
+    Link: ({ children }: { children: React.ReactNode }) => children,
+  };
+});
+
 describe('Landing Page', () => {
   it('Load the page correctly', () => {
     const { debug } = render(<Landing />);
