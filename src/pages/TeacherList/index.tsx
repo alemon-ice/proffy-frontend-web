090369e5cf3ceb15684b6e6b4706d39aafa3ecc7
Rafael Dias <rafaeldiasdesouza.rd@gmail.com>
2020-08-06 21:36:14 -0300

    feat: corrections

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -11,28 +11,21 @@ const TeacherList: React.FC = () => {
       <PageHeader title="Estes são os Proffys disponíveis">
         <form action="" id="search-teachers">
           <div className="input-block">
-            <label htmlFor="subject">
-              Matéria
-              </label>
+            <label htmlFor="subject">Matéria</label>
             <input type="text" id="subject" />
           </div>
           <div className="input-block">
-            <label htmlFor="week-day">
-              Dia da semana
-              </label>
+            <label htmlFor="week-day">Dia da semana</label>
             <input type="text" id="week-day" />
           </div>
           <div className="input-block">
-            <label htmlFor="time">
-              Hora
-              </label>
+            <label htmlFor="time">Hora</label>
             <input type="text" id="time" />
           </div>
         </form>
       </PageHeader>
       <TeacherItem />
-      <main>
-      </main>
+      <main />
     </div>
   );
 };
