a7c54c01b7980fadcdf3d16e28aeec9fc3aa51d8
Rafael Dias <rafaeldiasdesouza.rd@gmail.com>
2020-08-09 00:15:48 -0300

    feat: add tests to TeacherForm page

---
+++ src/__tests__/pages/TeacherForm.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { TeacherForm } from 'components/pages';
+
+import { render } from '@testing-library/react';
+
+jest.mock('react-router-dom', () => {
+  return {
+    Link: ({ children }: { children: React.ReactNode }) => children,
+  };
+});
+
+describe('Landing Page', () => {
+  it('Load the page correctly', () => {
+    const { debug } = render(<TeacherForm />);
+
+    debug();
+  });
+});
