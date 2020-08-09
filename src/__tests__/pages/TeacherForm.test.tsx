e4aa64932e12a200cae26732fe120a9fa495e4d0
Rafael Dias <rafaeldiasdesouza.rd@gmail.com>
2020-08-09 18:43:23 -0300

    feat: testing TeacherForm

--- src/__tests__/pages/TeacherForm.test.tsx
+++ src/__tests__/pages/TeacherForm.test.tsx
@@ -5,6 +5,7 @@ import { render } from '@testing-library/react';
 
 jest.mock('react-router-dom', () => {
   return {
+    useHistory: jest.fn(),
     Link: ({ children }: { children: React.ReactNode }) => children,
   };
 });
